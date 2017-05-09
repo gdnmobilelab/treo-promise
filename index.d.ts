57fc9db53adf837720515b19a62ad88103bc5cdb
Alastair Coote <alastair.coote@gmail.com>
2017-05-09 17:32:47 -0400

    Add clear() to promise definition file

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,19 @@
+declare class TreoPromiseInstance {
+    store(name: string): TreoPromiseStore;
+    version: number;
+}
+
+declare class TreoPromiseStore {
+    get<T>(key: string): Promise<T>;
+    put<T>(obj: T): Promise<void>;
+    del(key: string): Promise<void>;
+    all<T>(): Promise<T[]>;
+    clear(): Promise<void>;
+}
+
 declare module "treo-promise" {
-
-    interface TreoPromiseStore {
-        get<T>(key: string): Promise<T>;
-        put<T>(obj: T): Promise<void>;
-        del(key: string): Promise<void>;
-        all<T>(): Promise<T[]>;
-    }
-
-    class TreoPromiseInstance {
-        store(name: string): TreoPromiseStore;
-        version: number;
-    }
 
     export default function treoPromise(): TreoPromiseInstance;
 
 }
+
